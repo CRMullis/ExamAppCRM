8082f8ced1d4007eb4db4ec52ad23b7ef7003c0c
Collin Mullis <cmullis@saintjoe.edu>
2015-04-08 16:14:07 -0400

    Altered program to match the instructions given on Exam.

--- ExamTwo/src/edu/saintjoe/cs/brianc/examtwo/ExamActivity.java
+++ ExamTwo/src/edu/saintjoe/cs/brianc/examtwo/ExamActivity.java
@@ -1,7 +1,14 @@
+/* Commented and Edited by Collin Mullis
+ * Code Originally coded up by Brian Capouch
+ * CMP112 Practicum, #2
+ */
 
+//package name so that if this app was to go on the app store,
+// It would not have a problem with deleting someone else's app
+// with the same name
 package edu.saintjoe.cs.brianc.examtwo;
 
-
+// Imports components that this app uses
 import com.google.devtools.simple.runtime.components.Component;
 import com.google.devtools.simple.runtime.components.HandlesEventDispatching;
 import com.google.devtools.simple.runtime.components.android.Form;
@@ -10,13 +17,12 @@ import com.google.devtools.simple.runtime.components.android.Button;
 import com.google.devtools.simple.runtime.components.android.Label;
 import com.google.devtools.simple.runtime.components.android.HorizontalArrangement;
 import com.google.devtools.simple.runtime.components.android.TextBox;
-import com.google.devtools.simple.runtime.components.android.AccelerometerSensor;
 
 import com.google.devtools.simple.runtime.events.EventDispatcher;
-
+// Establish class, which is the Application itself
 public class ExamActivity extends Form implements HandlesEventDispatching {
-
-	private HorizontalArrangement line1, line2, line3, line4, line5;
+// Defines Components
+	private HorizontalArrangement line1, line2, line3;
 	private Button incButton;
 	private Label resultLabel;
 	private Label promptLabel;
@@ -24,45 +30,44 @@ public class ExamActivity extends Form implements HandlesEventDispatching {
 	private Label outputLabel;
 	int temp;
 	
-
+// This line is where the app runs, takes the place of main()
  void $define() {
- 	
+// Sets backgorund color to "White" 	
      this.BackgroundColor(COLOR_WHITE);
-     
+// This gives each HorizontalArragement a name.    
      line1 = new HorizontalArrangement(this);
      line2 = new HorizontalArrangement(this);
      line3 = new HorizontalArrangement(this);
-     line4 = new HorizontalArrangement(this);
 
-     
+// Sets prompt label to be in line1, and to have the text of "Enter a number:"     
      promptLabel = new Label(line1, "Enter a number:");
+// Sets inputBox on line1
      inputBox = new TextBox(line1);
+// Makes the inputBox only accept numeric values.
      inputBox.NumbersOnly(true);
-   
-     incButton = new Button(line2,"Double It"); 
      
+     
+// Sets incButton on line2 and sets its text to "Double It"
+     incButton = new Button(line2,"Double It"); 
+// Sets resultLabel on line 3 and sets it's text to blank     
      resultLabel = new Label(line3,"");
      
-     outputLabel = new Label(line4, "");
- 
+// Tells the app to look for whenever any button is pressed. 
      EventDispatcher.registerEventForDelegation(this, "ButtonClick", "Click");
     
  } 
  @Override
+ // Method for the class, which is also an event handler.
  public boolean dispatchEvent(Component component, String id, String eventName,
          Object[] args) {
- 	
+ 	// If incButton is clicked, turn the number into a string, multiply it by 2, then turn it back to a string
+	// then return true, and set result label to the end value.
 	    if (component.equals(incButton) && eventName.equals("Click")){
 	    	temp = Integer.parseInt(inputBox.Text());
 	    	temp = temp * 2;
 	    	resultLabel.Text(String.valueOf(temp));
 	        return true;
 	     } 
-	    
-	    if (component.equals(resultLabel) && eventName.equals("Click")) {
-	    	outputLabel.Text("You pushed me!!");
-	    	return true;
-	    }
     return true;
 	} 
 } 
